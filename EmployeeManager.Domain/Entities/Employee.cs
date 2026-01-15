bf4d75328a03056570471254d4c0f4026ffed2b6
Bogdan <nikolaevbv92@gmail.com>
2026-01-16 02:06:07 +0300

    добавить сущность сотрудника

---
+++ EmployeeManager.Domain/Entities/Employee.cs
@@ -0,0 +1,50 @@
+using EmployeeManager.Domain.Rules;
+
+namespace EmployeeManager.Domain.Entities
+{
+    public class Employee
+    {
+        public int Id { get; private set; }
+        public string FirstName { get; private set; }
+        public string Surname { get; private set; }
+        public string Patronymic { get; private set; }
+        public string Address { get; private set; }
+        public string Department { get; private set; }
+        public DateTime DateOfBirth { get; private set; }
+        public string AboutMe { get; private set; }
+
+        private Employee() { }
+
+        public static Employee Create(
+            string firstName,
+            string surname,
+            string patronymic,
+            string address,
+            string department,
+            DateTime dateOfBirth,
+            string aboutMe
+        )
+        {
+            var employee = new Employee
+            {
+                FirstName = firstName,
+                Surname = surname,
+                Patronymic = patronymic,
+                Address = address,
+                Department = department,
+                DateOfBirth = dateOfBirth,
+                AboutMe = aboutMe
+            };
+
+            employee.Validate();
+
+            return employee;
+        }
+
+        private void Validate()
+        {
+            EmployeeValidationRules.ValidationFirstName(FirstName);
+            EmployeeValidationRules.ValidationSurname(Surname);
+        }
+    }
+}
